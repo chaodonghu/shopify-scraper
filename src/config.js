c2d23e8d4eb88e03614ecfc96fa607bbd4058184
chaodonghu <dojostan@gmail.com>
2021-12-31 21:24:44 -0500

    move config file to js, edit env example file

---
+++ src/config.js
@@ -0,0 +1,35 @@
+export const DISCORD_MESSAGE_SETTINGS = {
+  botImage: "https://memegenerator.net/img/images/300x300/72665487.jpg",
+  botName: "Shopify Release Monitor",
+  footerDescription: "",
+  footerImage: "",
+  timeOfNotification: true,
+};
+
+export const REQUEST_TIMING = 30000;
+
+export const SITES = [
+  {
+    url: "deadstock.ca",
+    keywords: [["Nike"], ["Adidas", "Yeezy"], ["Jordan"]],
+  },
+  {
+    url: "capsuletoronto.com",
+    keywords: [["Nike"], ["Adidas", "Yeezy"], ["Jordan"]],
+  },
+  {
+    url: "nomadshop.net",
+    keywords: [["Nike"], ["Adidas", "Yeezy"], ["Jordan"]],
+  },
+  {
+    url: "ca.octobersveryown.com",
+  },
+  {
+    url: "nrml.ca",
+    keywords: [["Nike"], ["Adidas", "Yeezy"], ["Jordan"]],
+  },
+  {
+    url: "shop.havenshop.com",
+    keywords: [["Nike"], ["Adidas", "Yeezy"], ["Jordan"]],
+  },
+];
